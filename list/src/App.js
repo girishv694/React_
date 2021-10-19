1ee7059563db02b26c758c981761ec3ef5364253
Girishv <girishv694@gmail.com>
2021-10-19 21:53:23 +0530

    first

---
+++ list/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import List from './components/List'
+//import '../src/App.css'
+
+export class App extends Component {
+  render() {
+    return (
+     <div className="App">
+     <List/>
+     </div>
+    )
+  }
+}
+
+export default App
---
+++ list/src/components/List.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function List() {
+    return (
+        <div>
+            <h1>Mobile Operating Systems</h1>
+            <ul>
+            <li>Android</li>
+            <li>Blackberry</li>
+            <li>Iphone</li>
+            <li>windows phone</li>
+            </ul>
+
+
+
+
+            <h1>Mobile Manufacturers</h1>
+            <ul>
+            <li>Samsung</li>
+            <li>HTC</li>
+            <li>Micromax</li>
+            <li>Apple</li>
+            </ul>
+        </div>
+    )
+}
+
+export default List
