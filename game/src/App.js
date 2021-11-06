7c252d7c0d56c7231ab5470fb042bb27d6c01554
Girishv <girishv694@gmail.com>
2021-11-06 09:46:10 +0530

    Game

---
+++ game/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Game from './Game';
+import './TicTacToe.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Game/>
+    </div>
+  );
+}
+
+export default App;
