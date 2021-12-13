15f97776d057152c6e80da235ef2bed7ac2eef4a
Girishv <girishv694@gmail.com>
2021-12-13 16:42:51 +0530

    first resposnsive

--- redux/test-eval/src/App.js
+++ redux/test-eval/src/App.js
@@ -1,17 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import axios from 'axios'
 
 function App() {
 
-  const calling = () =>{
-    console.log("hi there")
+  const[message,setmessage] = useState("")
+
+  const[user,setUser] = useState({
+    firstname : '',
+    lastname : '',
+    dob:''
+  });
+
+  const {firstname,lastname,dob} =user; 
+  const inputchange = (e)=>{
+    setUser({
+      ...user,
+      [e.target.name] : e.target.value,
+    })
+  }
+
+  const submit= async (e)=>{
+    e.preventDefault();
+   try {
+      const response = await axios.post("http://localhost:3001/users",user);
+      setmessage("Registered successfully");
+       }catch(e){
+        setmessage("Something Wrong");
+       }
+
+    
+  //   if(!response){
+  //     setmessage("Something Wrong");
+  //   }
+  //   else
+  //  setmessage("Registered successfully");
+    
+
   }
   return (
     <div className="App">
+      
+      <div className="data">
+      <h1>User Registration</h1>
+      <form onSubmit={submit}> 
+        <input type="text" placeholder="firstname" name="firstname" value={firstname} 
+        onChange={(e)=>inputchange(e)}/><br/><br/>
+
+        <input type="text" placeholder="lastname" name="lastname" value={lastname}
+        onChange={(e)=>inputchange(e)} /><br/><br/>
+
+
+        <input type="text" placeholder="dob" name="dob" value={dob} 
+        onChange={(e)=>inputchange(e)}/><br/><br/>
+        <input type="submit" id="submit" value="Register user"/>
+      </form>
+      <p className="msg">{message}</p>
+
+      </div>
+      
+
      
-       
-        <input type="text"/>
-        <button Onclick={calling}>ADD here</button>
      
     </div>
   );
