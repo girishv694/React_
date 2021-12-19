1a39e3ef77b9d57f85acc0b1a2024c56ba2a7df4
Girishv <girishv694@gmail.com>
2021-12-19 21:32:08 +0530

    Test

---
+++ redux/test-app/cypress/integration/checkinput.spec.js
@@ -0,0 +1,19 @@
+/// <reference types="Cypress"/>
+
+describe("Checkging the input",() => {
+    beforeEach("Checking if the server is running or not",() => {
+        cy.visit("http://localhost:3000")
+    })
+    it("describe the input",() => {
+        cy.visit("http://localhost:3000")
+        cy.get(".inp").should("exist");
+        cy.get(".btn").should("exist");
+        cy.get(".inp").type("Learn React").should("have.value","Learn React");
+        cy.get(".btn").click();
+    })
+
+    // it("checking about the rmbtn",() => {
+    //     cy.visit("http://localhost:3000")
+    //     cy.get(".rmbtn").should("exist");
+    // })
+})
---
+++ redux/test-app/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import {Todo} from "./components/Todo";
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the cypress Testing  Toggle and Delete Assignment </h1>
+      <Todo/>
+    </div>
+  );
+}
+
+export default App;
