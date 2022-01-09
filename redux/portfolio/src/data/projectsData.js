8e3b30b3f9d6df86efbe10b29013092b4e2bdd4a
Girishv <girishv694@gmail.com>
2022-01-09 19:14:55 +0530

    Fourth project

---
+++ redux/portfolio/src/data/projectsData.js
@@ -0,0 +1,42 @@
+
+
+export const projectsData = [
+    {
+        id: 1,
+        projectName: 'Purplle Clone',
+        projectDesc: 'Purplle is an online store that sells cosmetics fragrances skin and hair care products.',
+        tags: ['HTML', 'CSS', 'javaScript', 'mongoDB', 'NPM', 'hbs'],
+        code: 'https://github.com/shaista78/Cloning_Website_Using_Backend',
+        demo: 'http://purplleclone.herokuapp.com/html/home.html',
+        image: 'https://user-images.githubusercontent.com/87421802/135706765-3bd3b680-fb72-497e-a4b2-8619bd026e3d.png'
+    },
+    {
+        id: 2,
+        projectName: 'Scavenger App',
+        projectDesc: 'Objective of this project is to buy and sell E waste scraps from users.',
+        tags: ['ReactJs','HTML', 'CSS', 'javaScript','MondoDB'],
+        code: 'https://github.com/girishv694/E-wasteManagement',
+        demo: 'https://e-waste-management.netlify.app/',
+        // image: 'https://e-waste-management.netlify.app/Images/page1Image.png'
+        image : 'https://e-waste-management.netlify.app/Images/scrap_sold.png'
+    },
+    
+    {
+        id: 4,
+        projectName: 'Ikea-clone',
+        projectDesc: 'Ikea is a online home decorative selling web application ',
+        tags: ['React', 'material-ui','Node js','Express Js','Firebase','MongoDB'],
+        code: 'https://github.com/girishv694/ikea-clone',
+        demo: 'https://ikea-clone-jan.netlify.app/',
+        image: 'https://ikea-clone-jan.netlify.app/images/homefrontimage.png'
+    },
+    {
+        id: 3,
+        projectName: 'Food App',
+        projectDesc: 'This application allow you to search your favourite foods.',
+        tags: ['HTML', 'CSS', 'JavaScript'],
+        code: 'https://github.com/girishv694/Unit-3/tree/master/Export',
+        demo: 'https://masai-course.s3.ap-south-1.amazonaws.com/users/1656/submissions/184958/357577/0134543f539a2e31e3542996b7ec6925/zoom_0.mp4',
+        image: 'https://media-cdn.tripadvisor.com/media/photo-s/12/ee/ab/bc/paneer-veggie-platter.jpg',
+    },
+]
